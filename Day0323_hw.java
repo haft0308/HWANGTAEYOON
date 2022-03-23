a0594b7a8b7447e267e7d1c7e453fb5b9e5fb3a8
TedHwang0224 <101791606+TedHwang0224@users.noreply.github.com>
2022-03-23 15:39:25 +0900

    Add files via upload

---
+++ Day0323_hw.java
@@ -0,0 +1,162 @@
+
+public class Day0323_hw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// int[]score = new int[]{0, 10, 20, 30, 40};
+		int[] a = {10, 20, 30, 40, 50};
+			for(int b = 0; b < 5; b++)
+			{
+				System.out.println("score["+b+"] = "+a[0]);
+			}
+
+			
+		
+		System.out.println("--------------------");
+		
+		
+		
+//		byte[] c = {1, 2, 3, 4, 5};
+//			for(byte d = 0; d < 5; d++)
+		byte[] c;
+		c = new byte[5];
+		c[0] = 1;
+		c[1] = 2;
+		c[2] = 3;
+		c[3] = 4;
+		c[4] = 5;
+		byte[] d = {1, 2, 3, 4, 5};
+		System.out.println("d[0] : "+d[0]);
+		System.out.println("d[1] : "+d[1]);
+		System.out.println("d[2] : "+d[2]);
+		System.out.println("d[3] : "+d[3]);
+		System.out.println("d[4] : "+d[4]);
+
+		
+		
+		System.out.println("--------------------");
+		
+
+		
+		char[] e;
+		e = new char[5];
+		e[0] = 'A';
+		e[1] = 'B';
+		e[2] = 'C';
+		e[3] = 'D';
+		e[4] = 'E';
+		char[] f = {'A', 'B', 'C', 'D', 'E'};
+		System.out.println("f[0] : "+f[0]);
+		System.out.println("f[1] : "+f[1]);
+		System.out.println("f[2] : "+f[2]);
+		System.out.println("f[3] : "+f[3]);
+		System.out.println("f[4] : "+f[4]);
+
+		
+		
+		System.out.println("--------------------");
+		
+		
+		
+		boolean[] g;
+		g = new boolean[5];
+		g[0] = true;
+		g[1] = false;
+		g[2] = true;
+		g[3] = false;
+		g[4] = true;
+		boolean[] h = {true, false, true, false, true};
+		System.out.println("h[0] : "+h[0]);
+		System.out.println("h[1] : "+h[1]);
+		System.out.println("h[2] : "+h[2]);
+		System.out.println("h[3] : "+h[3]);
+		System.out.println("h[4] : "+h[4]);
+		
+		
+		
+		System.out.println("--------------------");
+		
+		
+		
+		short[] i;
+		i = new short[5];
+		i[0] = 100;
+		i[1] = 200;
+		i[2] = 300;
+		i[3] = 400;
+		i[4] = 500;
+		short[] j = {100, 200, 300, 400, 500};
+		System.out.println("i[0] : "+i[0]);
+		System.out.println("i[1] : "+i[1]);
+		System.out.println("i[2] : "+i[2]);
+		System.out.println("i[3] : "+i[3]);
+		System.out.println("i[4] : "+i[4]);
+		
+		
+		
+		System.out.println("--------------------");
+		
+		
+		
+		long[] k;
+		k = new long[5];
+		k[0] = 90;
+		k[1] = 135;
+		k[2] = 180;
+		k[3] = 225;
+		k[4] = 270;
+		long[] l = {90, 135, 180, 225, 270};
+		System.out.println("k[0] : "+k[0]);
+		System.out.println("k[1] : "+k[1]);
+		System.out.println("k[2] : "+k[2]);
+		System.out.println("k[3] : "+k[3]);
+		System.out.println("k[4] : "+k[4]);
+		
+		
+		
+		System.out.println("--------------------");
+		
+		
+		
+		float[] m;
+		m = new float[5];
+		m[0] = (float) 3.14;
+		m[1] = (float) 6.28;
+		m[2] = (float) 9.42;
+		m[3] = (float) 12.56;
+		m[4] = (float) 15.700001;
+		float[] n = {(float) 3.14, (float) 6.28, (float) 9.42, (float) 12.56, (float) 15.700001};
+		System.out.println("n[0] : "+n[0]);
+		System.out.println("n[1] : "+n[1]);
+		System.out.println("n[2] : "+n[2]);
+		System.out.println("n[3] : "+n[3]);
+		System.out.println("n[4] : "+n[4]);
+		
+		
+		
+		System.out.println("--------------------");
+		
+		
+		
+		double[] o;
+		o = new double[5];
+		o[0] = 3.141592;
+		o[1] = 3.141592;
+		o[2] = 3.141592;
+		o[3] = 3.141592;
+		o[4] = 3.141592;
+		double[] p = {3.141592, 3.141592, 3.141592, 3.141592, 3.141592};
+		System.out.println("o[0] : "+o[0]);
+		System.out.println("o[1] : "+o[1]);
+		System.out.println("o[2] : "+o[2]);
+		System.out.println("o[3] : "+o[3]);
+		System.out.println("o[4] : "+o[4]);
+		
+		
+		
+		System.out.println("--------------------");
+		
+	}
+
+}
